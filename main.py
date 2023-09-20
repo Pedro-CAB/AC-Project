ac63f6a20f730eaca95b46614d781e0e9dcd6bd7
Pedro-CAB <up202006086@up.pt>
2023-09-20 11:03:10 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+awards_players = pd.read_csv('dataset/awards_players.csv')
+coaches = pd.read_csv('dataset/coaches.csv')
+players = pd.read_csv('dataset/players.csv')
+players_teams = pd.read_csv('dataset/players_teams.csv')
+series_post = pd.read_csv('dataset/series_post.csv')
+teams = pd.read_csv('dataset/teams.csv')
+teams_post  = pd.read_csv('dataset/teams_post.csv')
